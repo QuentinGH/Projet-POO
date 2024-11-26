546a628015f287b9e3da8a10d7418ffe074ba458
QuentinGH <quentin.chabres@gmail.com>
2024-11-26 10:42:41 +0100

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -1,5 +1,34 @@
-//
-// Created by quent on 26/11/2024.
-//
-
 #include "Cell.h"
+
+Cell::Cell() {
+    this->x = 0;
+    this->y = 0;
+    this->alive = 0;
+}
+
+Cell::Cell(int nbrx,int nbry) {
+    this->x = nbrx;
+    this->y = nbry;
+}
+
+int Cell::get_x() { 
+    return this->x; 
+}    
+
+int Cell::get_y() { 
+    return this->y; 
+}
+
+int Cell::set_x(int nbr) { 
+    this->x = nbr;
+    return x; 
+}
+
+int Cell::set_y(int nbr) { 
+    this->y = nbr;
+    return y; 
+}
+
+bool Cell::get_alive() {
+    return this->alive;
+}
