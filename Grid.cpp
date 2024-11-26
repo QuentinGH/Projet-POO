ed5de7e75d4a5e81a6f08ebc0721577225ab59c9
QuentinGH <quentin.chabres@gmail.com>
2024-11-26 10:41:52 +0100

    Update Grid.cpp

--- Grid.cpp
+++ Grid.cpp
@@ -1,5 +1,29 @@
-//
-// Created by quent on 26/11/2024.
-//
-
 #include "Grid.h"
+
+Grid::Grid() {
+    this->height = 0;
+    this->width = 0;
+}
+
+Grid::Grid(int nbrheight,int nbrwidth) {
+    this->height = nbrheight;
+    this->width = nbrwidth;
+}
+
+int Grid::get_height() { 
+    return this->height; 
+}    
+
+int Grid::get_width() { 
+    return this->width; 
+}
+
+int Grid::set_height(int nbr) { 
+    this->height = nbr;
+    return height; 
+}
+
+int Grid::set_width(int nbr) { 
+    this->width = nbr;
+    return width; 
+}
