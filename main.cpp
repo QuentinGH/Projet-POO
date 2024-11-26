b92f7d910cb0ab17ac98e28bfed757f28bce86fc
QuentinGH <quentin.chabres@gmail.com>
2024-11-26 10:58:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,25 @@
 #include <sstream>
 #include <iomanip>
 #include <utility>
+#include "Grid.h"
 
 int main() {
-    std::cout << "You may enter the value of the grid's height and width (one after the other with a space between them): " << std::endl;
+    Grid grille;
     std::pair<int, int> values;
-    
-    std::string line;
-    std::getline(std::cin, line); // Lire toute la ligne d'entrée
-    
-    std::stringstream ss(line); // Créer un flux de chaîne pour analyser la ligne
-    ss >> values.first >> values.second; // Extraire les deux entiers
-    
+
+    while (values.first <=0 && values.second <= 0) {
+        std::cout << "You may enter the value of the grid's height and width (one after the other with a space between them): " << std::endl;
+
+        std::string line;
+        std::getline(std::cin, line); // Lire toute la ligne d'entrée
+
+        std::stringstream ss(line); // Créer un flux de chaîne pour analyser la ligne
+        ss >> values.first >> values.second; // Extraire les deux entiers
+    }
+
     std::cout << "\nYour values are :"
          << "\n- Height : " << values.first
          << "\n- Width : " << values.second << std::endl;
+
     return 0;
 }
