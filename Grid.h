75b5b5b54075e7dec84cff3607288bd0d17d2db1
QuentinGH <quentin.chabres@gmail.com>
2024-11-28 09:51:55 +0100

    Update Grid.h

--- Grid.h
+++ Grid.h
@@ -1,12 +1,13 @@
 #ifndef GRID_H
 #define GRID_H
-
+#include <vector>
 
 
 class Grid {
 private:
     int height;
     int width;
+    std::vector<std::vector<bool>> map;
 
 public:
 
@@ -15,8 +16,9 @@ public:
 
     int get_height() { return height; }
     int get_width() { return width; }
-    int set_height(int nbr) { return height; }
-    int set_width(int nbr) { return width; }
+    int set_height(int nbr);
+    int set_width(int nbr);
+    void Affichemap();
 };
 
 
