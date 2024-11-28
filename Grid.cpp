2a5c3047e5aa95d0b21a8256c8dfdb0d699895cc
QuentinGH <quentin.chabres@gmail.com>
2024-11-28 09:52:56 +0100

    Update Grid.cpp

--- Grid.cpp
+++ Grid.cpp
@@ -1,21 +1,16 @@
 #include "Grid.h"
+#include <iostream>
 
 Grid::Grid() {
     this->height = 0;
     this->width = 0;
+    this->map = std::vector<std::vector<bool>>(this->height, std::vector<bool>(this->width, false));
 }
 
 Grid::Grid(int nbrheight,int nbrwidth) {
     this->height = nbrheight;
     this->width = nbrwidth;
-}
-
-int Grid::get_height() { 
-    return this->height; 
-}    
-
-int Grid::get_width() { 
-    return this->width; 
+    this->map = std::vector<std::vector<bool>>(this->height, std::vector<bool>(this->width, false));
 }
 
 int Grid::set_height(int nbr) { 
@@ -26,4 +21,14 @@ int Grid::set_height(int nbr) {
 int Grid::set_width(int nbr) { 
     this->width = nbr;
     return width; 
+}
+
+void Grid::Affichemap() {
+    for (size_t i = 0; i < map.size(); i++) {
+        for (size_t j = 0; j < map[i].size(); j++) {
+            std::cout << map[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << std::endl;
 }
