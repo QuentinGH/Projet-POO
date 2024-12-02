938340e22d7457c52b97f899a66d46daaafeb9c6
QuentinGH <quentin.chabres@gmail.com>
2024-12-02 10:20:20 +0100

    Update Grid.cpp

--- Grid.cpp
+++ Grid.cpp
@@ -4,29 +4,37 @@
 Grid::Grid() {
     this->height = 0;
     this->width = 0;
-    this->map = std::vector<std::vector<bool>>(this->height, std::vector<bool>(this->width, false));
+    this->Gmap = std::vector<std::vector<bool>>(this->height, std::vector<bool>(this->width, false));
 }
 
 Grid::Grid(int nbrheight,int nbrwidth) {
     this->height = nbrheight;
     this->width = nbrwidth;
-    this->map = std::vector<std::vector<bool>>(this->height, std::vector<bool>(this->width, false));
+    this->Gmap = std::vector<std::vector<bool>>(this->height, std::vector<bool>(this->width, false));
 }
 
-int Grid::set_height(int nbr) { 
-    this->height = nbr;
-    return height; 
+int Grid::set_height(int nbr) {
+    return this->height = nbr;
 }
 
-int Grid::set_width(int nbr) { 
-    this->width = nbr;
-    return width; 
+int Grid::set_width(int nbr) {
+    return this->width = nbr;
+}
+
+std::vector<std::vector<bool>> Grid::modify(int x, int y, bool b) {
+    if (x >= 0 && x < height && y >= 0 && y < width) {
+        this->Gmap[x][y] = b; // Modify the value at position (x, y)
+    }
+    else {
+        throw std::runtime_error("Erreur : mauvaises coordonées");
+    }
+    return Gmap;
 }
 
 void Grid::Affichemap() {
-    for (size_t i = 0; i < map.size(); i++) {
-        for (size_t j = 0; j < map[i].size(); j++) {
-            std::cout << map[i][j] << " ";
+    for (size_t i = 0; i < Gmap.size(); i++) {
+        for (size_t j = 0; j < Gmap[i].size(); j++) {
+            std::cout << Gmap[i][j] << " ";
         }
         std::cout << "\n";
     }
