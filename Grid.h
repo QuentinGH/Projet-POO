d7f71f4f4a636f1fe216a64331374fa5921b37a4
QuentinGH <quentin.chabres@gmail.com>
2024-12-03 16:49:38 +0100

    Update Grid.h

--- Grid.h
+++ Grid.h
@@ -2,13 +2,14 @@
 #define GRID_H
 #include <vector>
 #include <string>
-
+#include <stack>
 
 class Grid {
 private:
     int height;
     int width;
     std::vector<std::vector<bool>> Gmap;
+    std::stack<std::vector<std::vector<bool>>> sauvegardes; // Pile pour sauvegardes successives
 
 public:
     Grid();
@@ -16,12 +17,14 @@ public:
 
     int get_height() { return height; }
     int get_width() { return width; }
-    std::vector<std::vector<bool>>& get_Gmap() { return Gmap;}
+    std::vector<std::vector<bool>>& get_Gmap() { return Gmap; }
     int set_height(int nbr);
     int set_width(int nbr);
     void Affichemap();
+
+    // Sauvegarde et chargement
+    void sauvegarder(const std::string& nom_fichier);
+    void charger(const std::string& nom_fichier);
 };
-
-
 
 #endif //GRID_H
