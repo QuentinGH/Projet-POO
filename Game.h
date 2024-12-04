ba7ce5a336675c78b02aeb25ee8cd48a4c465677
Wakœur <114169151+Wakoeur@users.noreply.github.com>
2024-12-04 11:51:58 +0100

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,4 +1,5 @@
 #ifndef GAME_H
+#ifndef GAME_H
 #define GAME_H
 
 #include "Cell.h"
@@ -19,6 +20,10 @@ public:
     Cell& getCell(int i, int j);
     void modify(int x, int y, bool b);
     void afficherCell(int i, int j);
+    void syncGmapWithTab();
+    void syncTabWithGmap();
+    void sauvegarder(const std::string& nom_fichier);
+    void charger(const std::string& nom_fichier);
     int detection(Cell &c);
     void behavior(int x, int y);
     int toricX(int x);
