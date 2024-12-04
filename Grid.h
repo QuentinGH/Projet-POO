6ea08c1a3482926f80d25b812b9b57d2f4aba3be
QuentinGH <quentin.chabres@gmail.com>
2024-12-04 11:25:00 +0100

    Update Grid.h

--- Grid.h
+++ Grid.h
@@ -1,30 +1,40 @@
 #ifndef GRID_H
 #define GRID_H
+
 #include <vector>
 #include <string>
 #include <stack>
+#include "Cell.h"
 
 class Grid {
 private:
     int height;
     int width;
-    std::vector<std::vector<bool>> Gmap;
-    std::stack<std::vector<std::vector<bool>>> sauvegardes; // Pile pour sauvegardes successives
+    std::vector<std::vector<Cell*>> Gmap;
+    std::stack<std::vector<std::vector<Cell>>> sauvegardes;
+
+    void clearGmap();
 
 public:
     Grid();
     Grid(int nbrheight, int nbrwidth);
+    ~Grid();
 
-    int get_height() { return height; }
-    int get_width() { return width; }
-    std::vector<std::vector<bool>>& get_Gmap() { return Gmap; }
+    Grid(const Grid& other);
+    Grid& operator=(const Grid& other);
+    int get_height() const { return height; }
+    int get_width() const { return width; }
+    std::vector<std::vector<Cell*>>& get_Gmap() { return Gmap; }
+
     int set_height(int nbr);
     int set_width(int nbr);
+    void modify(int x, int y, bool b);
     void Affichemap();
+    Cell& getCell(int i, int j);
+    void afficherCell(int i, int j);
 
-    // Sauvegarde et chargement
     void sauvegarder(const std::string& nom_fichier);
     void charger(const std::string& nom_fichier);
 };
 
-#endif //GRID_H
+#endif // GRID_H
