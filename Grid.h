50b595cfcf3edea552baaf61341fa4643318a6d0
Wakœur <114169151+Wakoeur@users.noreply.github.com>
2024-12-04 17:02:40 +0100

    Update Grid.h

--- Grid.h
+++ Grid.h
@@ -1,25 +1,44 @@
 #ifndef GRID_H
 #define GRID_H
+
 #include <vector>
 #include <string>
+#include <Stack>
+#include "Cell.h"
 
 
 class Grid {
 private:
     int height;
     int width;
-    std::vector<std::vector<bool>> Gmap;
+    std::vector<std::vector<Cell*>> Gmap;
+    std::stack<std::vector<std::vector<Cell>>> sauvegardes;
+
+    void clearGmap();
 
 public:
     Grid();
     Grid(int nbrheight, int nbrwidth);
+    ~Grid();
 
+    Grid(const Grid& other);
+    Grid& operator=(const Grid& other);
     int get_height() { return height; }
     int get_width() { return width; }
-    std::vector<std::vector<bool>>& get_Gmap() { return Gmap;}
+    std::vector<std::vector<Cell*>>& get_Gmap() { return Gmap;}
+
     int set_height(int nbr);
     int set_width(int nbr);
+    void modify(int x, int y, bool b);
     void Affichemap();
+    Cell& getCell(int i, int j);
+    void afficherCell(int i, int j);
+
+    int detection(Cell &c);
+    void behavior(int x, int y);
+
+    void sauvegarder(const std::string& nom_fichier);
+    void charger(const std::string& nom_fichier);
 };
 
 
