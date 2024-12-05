7ce7de792bbbc6abfe4daf4d80566195374e51bf
QuentinGH <quentin.chabres@gmail.com>
2024-12-05 10:28:51 +0100

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -4,25 +4,28 @@ Cell::Cell() {
     this->x = 0;
     this->y = 0;
     this->alive = 0;
+    this->next = 0;
 }
 
 Cell::Cell(int nbrx,int nbry) {
     this->x = nbrx;
     this->y = nbry;
     this->alive = 0;
+    this->next = 0;
 }
 
-int Cell::set_x(int nbr) { 
-    this->x = nbr;
-    return x; 
+void Cell::set_x(int nbr) {
+    x = nbr;
 }
 
-int Cell::set_y(int nbr) { 
-    this->y = nbr;
-    return y; 
+void Cell::set_y(int nbr) {
+    y = nbr;
 }
 
-bool Cell::set_alive(bool b) {
+void Cell::set_alive(bool b) {
     this->alive = b;
-    return this->alive;
+}
+
+void Cell::set_alive_next(bool b) {
+    this->next = b;
 }
