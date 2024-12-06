3038d4c8bf804719fc878f8a2693a646349d9f96
Wakœur <114169151+Wakoeur@users.noreply.github.com>
2024-12-06 15:06:44 +0100

    Update Grid.h

--- Grid.h
+++ Grid.h
@@ -30,8 +30,9 @@ public:
 
     int set_height(int nbr);
     int set_width(int nbr);
-    void modify(int x, int y, bool b, std::vector<std::vector<Cell*>> &copy);
     void Affichemap();
+    void modify(int x, int y, bool b);
+    void update();
     Cell& getCell(int i, int j);
     void afficherCell(int i, int j);
 
