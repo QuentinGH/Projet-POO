af2a86b98709b9271354aa6f9eaddb983a15dd55
Wakœur <114169151+Wakoeur@users.noreply.github.com>
2024-12-07 03:03:20 +0100

    Update CL_Window.cpp

--- CL_Window.cpp
+++ CL_Window.cpp
@@ -37,40 +37,83 @@ void CL_Window::set_size_cell(int h, int w) {
 
 void CL_Window::eternity(Grid& jeu) {
     sf::RenderWindow wind(sf::VideoMode(this->size_w, this->size_h), "Life's Game");
+
+    sf::RectangleShape square(sf::Vector2f((this->height * this->size_cell) / this->height, (this->width * this->size_cell) / this->width));
+    square.setOutlineColor(sf::Color(200, 200, 200));
+    square.setOutlineThickness(this->size_outline);
+
+    int user_x = 0;
+    int user_y = 0;
+    sf::RectangleShape user(sf::Vector2f(((this->height*0.60) * this->size_cell) / this->height, ((this->width*0.60) * this->size_cell) / this->width));
+    user.setFillColor(sf::Color::Green);
+    user.setOutlineColor(sf::Color(200, 200, 200));
+    user.setOutlineThickness(this->size_outline);
+    user.setPosition(this->size_cell * user_x, this->size_cell * user_y);
+
+    int time = 501;
     int counter = 1;
+    bool run = false;
     while (wind.isOpen()) {
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && user_x > 0) {user_x--;}
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && user_x < this->width - 1) {user_x++;}
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && user_y > 0) {user_y--;}
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && user_y < this->height - 1) {user_y++;}
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            jeu.modify(user_y, user_x-1, 1);
+            jeu.modify(user_y+1, user_x, 1);
+            jeu.modify(user_y+1, user_x+1, 1);
+            jeu.modify(user_y, user_x+1, 1);
+            jeu.modify(user_y-1, user_x+1, 1);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            jeu.modify(user_y, user_x, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::F)) {
+            time -= 100;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            time += 100;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::P)) {
+            run = true;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::B)) {
+            run = false;
+        }
 
         wind.setFramerateLimit(60);
         sf::Event event;
         while (wind.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
                 wind.close();
         }
 
-        sf::RectangleShape square(sf::Vector2f((this->height * this->size_cell) / this->height, (this->width * this->size_cell) / this->width));
-        square.setOutlineColor(sf::Color::Black);
-        square.setOutlineThickness(this->size_outline);
+        if (run) {
+            wind.clear();
+            for (int count_1 = 0; count_1 < this->height; count_1++) {
+                for (int count_2 = 0; count_2 < this->width; count_2++) {
+                    square.setPosition(this->size_cell * count_2, this->size_cell * count_1);
 
-        wind.clear();
-        for (int count_1 = 0; count_1 < this->height; count_1++) {
-            for (int count_2 = 0; count_2 < this->width; count_2++) {
-                square.setPosition(this->size_cell * count_2, this->size_cell * count_1);
-
-                if (jeu.get_Gmap()[count_1][count_2]->get_alive() == true) {
-                    square.setFillColor(sf::Color::Black);
+                    if (jeu.get_Gmap()[count_1][count_2]->get_alive() == true) {
+                        square.setFillColor(sf::Color::White);
+                    }
+                    else {
+                        square.setFillColor(sf::Color::Black);
+                    }
+                    wind.draw(square);
+                    jeu.behavior(count_1, count_2);
                 }
-                else {
-                    square.setFillColor(sf::Color::White);
-                }
-                wind.draw(square);
-                jeu.behavior(count_1, count_2);
             }
+            user.setPosition(this->size_cell * user_x, this->size_cell * user_y);
+            wind.draw(user);
+            jeu.update();
+            jeu.sauvegarder("C:/Users/guerr/CLionProjects/SFML_test/grille_out");
+            wind.display();
+            jeu.Affichemap();
+            std::cout << counter++ << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(time));
         }
-        jeu.update();
-        jeu.sauvegarder("C:/Users/guerr/CLionProjects/SFML_test/grille_out");
-        wind.display();
-        jeu.Affichemap();
-        std::cout << counter++ << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
 }
