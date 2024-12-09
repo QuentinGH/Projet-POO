fbb9ad8b793f8958c827ec771bc346d544ab626e
Wakœur <114169151+Wakoeur@users.noreply.github.com>
2024-12-09 08:40:09 +0100

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -1,28 +1,32 @@
+
 #include "Cell.h"
 
 Cell::Cell() {
     this->x = 0;
     this->y = 0;
     this->alive = 0;
+    this->next = 0;
 }
 
 Cell::Cell(int nbrx,int nbry) {
     this->x = nbrx;
     this->y = nbry;
     this->alive = 0;
+    this->next = 0;
 }
 
-int Cell::set_x(int nbr) { 
-    this->x = nbr;
-    return x; 
+void Cell::set_x(int nbr) {
+    x = nbr;
 }
 
-int Cell::set_y(int nbr) { 
-    this->y = nbr;
-    return y; 
+void Cell::set_y(int nbr) {
+    y = nbr;
 }
 
-bool Cell::set_alive(bool b) {
+void Cell::set_alive(bool b) {
     this->alive = b;
-    return this->alive;
+}
+
+void Cell::set_alive_next(bool b) {
+    this->next = b;
 }
